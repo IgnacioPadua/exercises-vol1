7a1058e707d30cc2f428538ed4ee515b8c78fcf5
IgnacioPadua <nacho.ph@gmail.com>
2022-06-28 01:13:01 -0500

    Add: Exercise #4

---
+++ algo004.py
@@ -0,0 +1,12 @@
+# Lea de un mismo registro el nombre y la edad  de cualquier persona
+# e imprima sólo si la persona es mayor de edad, el nombre de la persona, 
+# de lo contrario, imprima un mensaje que diga : "no puede votar"
+
+
+nombre = 'Ignacio'
+edad = 18
+
+if edad >= 18:
+    print(nombre)
+else:
+    print('No puede votar.')
