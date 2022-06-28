0705d682329050dbbf0d4d2144230ed5043c4d4f
IgnacioPadua <nacho.ph@gmail.com>
2022-06-28 01:19:14 -0500

    Add: Exercise #18

---
+++ algo018.py
@@ -0,0 +1,6 @@
+# Leer un número y calcular el 5% del número leído. Obtener tanto el
+# número como el porcentaje calculado.
+
+numero1 = 50
+porcentaje = numero1 * .05
+print('El 5% de ' + str(numero1) + ' es: ' + str(porcentaje))
