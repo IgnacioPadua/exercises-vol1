32357d2cfb7164f9acb1cf6255dad3a98186421b
IgnacioPadua <nacho.ph@gmail.com>
2022-06-28 01:13:34 -0500

    Add: Exercise #8

---
+++ algo008.py
@@ -0,0 +1,6 @@
+# Lea un número e imprima el valor absoluto del mismo
+
+numero = -17
+
+numero_absoluto = abs(numero)
+print('El número es: ' + str(numero) + ' y su valor absoluto es: ' + str(numero_absoluto))
