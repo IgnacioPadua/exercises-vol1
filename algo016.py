238e07c3f9502b0c49785d329a4f507f9d868189
IgnacioPadua <nacho.ph@gmail.com>
2022-06-28 01:18:59 -0500

    Add: Exercise #16

---
+++ algo016.py
@@ -0,0 +1,13 @@
+# Lea dos números de un mismo registro y calculé el cociente de dividir
+# el primero por el segundo. Imprima el cociente. Si el segundo número es 
+# cero no ejecute el cálculo e imprima un mensaje que diga "La división no
+# es posible "
+
+dividendo = 3478
+divisor = 26
+
+if divisor == 0:
+    print('La división no es posible.')
+else:
+    cociente = dividendo / divisor
+    print(cociente)
