f0dba2c2d09aeb5b59c9c1b3b9cb22f105074c47
IgnacioPadua <nacho.ph@gmail.com>
2022-06-28 01:12:13 -0500

    Add: Exercise #2

---
+++ algo002.py
@@ -0,0 +1,7 @@
+# Lea de un mismo registro  el nombre y la edad de cualquier persona,
+# e imprima tanto el nombre como la edad
+
+nombre = 'nacho'
+edad = 40
+
+print('Su nombre es: '+ nombre + ' y su edad es de: ' + str(edad) + ' años.')
