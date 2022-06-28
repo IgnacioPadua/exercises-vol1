c2cc1b4d8303213491bd2b854d148e1d9852935d
IgnacioPadua <nacho.ph@gmail.com>
2022-06-28 01:18:37 -0500

    Add: Exercise #12

---
+++ algo012.py
@@ -0,0 +1,8 @@
+# Lea dos números de un mismo registro e imprima ambos números
+# si por lo menos uno de ellos es positivo.
+
+numero_uno = 10
+numero_dos = -12
+
+if numero_uno > 0 or numero_dos > 0:
+    print(numero_uno, numero_dos)
