bce02d3347e12ade3f093b0158d6cab50c13023a
IgnacioPadua <nacho.ph@gmail.com>
2022-06-28 01:18:47 -0500

    Add: Exercise #14

---
+++ algo014.py
@@ -0,0 +1,8 @@
+# Lea dos números  de un mismo registro. Calcule la suma e imprima
+# los números leídos y la suma.
+
+numero1 = 10
+numero2 = 1
+
+resultado_suma = numero1 + numero2
+print('La suma de ' + str(numero1) + ' + ' + str(numero2) + ' es: ' + str(resultado_suma))
