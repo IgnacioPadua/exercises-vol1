54d646e20dfaca1bb5b7754c37e88a16e1daf780
IgnacioPadua <nacho.ph@gmail.com>
2022-06-28 01:19:06 -0500

    Add: Exercise #17

---
+++ algo017.py
@@ -0,0 +1,10 @@
+# Leer de un mismo registro el nombre de un empleado, su salario básico 
+# por hora y el número de horas trabajadas en el mes. Calcular su salario
+# mensual e imprimir tanto el nombre como su salario mensual.
+
+nombre_empleado = 'Nacho'
+salario_por_hora = 200
+horas_trabajadas_mensual = 32
+
+salario_mensual = salario_por_hora * horas_trabajadas_mensual
+print('El salario mensual de ' + nombre_empleado + ' es de: ' + str(salario_mensual))
