a6dd99d573ddd605239bf982b33f1e3b2e40a8bb
IgnacioPadua <nacho.ph@gmail.com>
2022-06-28 01:12:21 -0500

    Add: Exercise #3

---
+++ algo003.py
@@ -0,0 +1,9 @@
+# Lea de un mismo registo  el nombre y la edad de cualquier persona
+# e imprima,  solo si la persona es mayor de edad, 
+# el nombre de la persona.
+
+nombre = 'Ignacio'
+edad = 18
+
+if edad >= 18:
+    print('Su nombre es: ' + nombre )
