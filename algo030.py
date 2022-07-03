1e6b9453ab9e7c261c0408ac69123c75ee83d495
IgnacioPadua <nacho.ph@gmail.com>
2022-07-02 20:06:27 -0500

    Add: Exercise #30

---
+++ algo030.py
@@ -0,0 +1,12 @@
+'''
+30. Obtener la suma de los números pares comprendidos entre 1 y 100.
+'''
+
+j=0
+for i in range(1,101):
+    saber_par = i % 2
+    if saber_par == 0:
+        j = i + j
+        #print(f'El valor de i es: {i} y de j es: {j}')
+
+print(f'La suma de los números pares entre 1 y 100 es: {j}')
