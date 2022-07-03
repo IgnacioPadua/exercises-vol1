ec07974178c1353055e23824f5be56dc6143510c
IgnacioPadua <nacho.ph@gmail.com>
2022-07-02 21:09:11 -0500

    MODIFY: Exercise #30

--- algo030.py
+++ algo030.py
@@ -6,7 +6,7 @@ j=0
 for i in range(1,101):
     saber_par = i % 2
     if saber_par == 0:
-        j = i + j
+        j+=i
         #print(f'El valor de i es: {i} y de j es: {j}')
 
 print(f'La suma de los números pares entre 1 y 100 es: {j}')
